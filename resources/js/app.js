4e4bb8e706b893d634202639d175155714bdd690
hilmanski <hilman@hilman.com>
2020-09-17 14:30:49 +0800

    prepare js and css webpack

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+//custom selector
+window.$ = function $(el) {
+    return (el.charAt[0] == "#")
+        ? document.querySelector(el)
+        : document.querySelectorAll(el)
+}
+
+//axios
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
