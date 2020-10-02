f3d6f613dbe623b4e27ab42038a9efc6a975350b
hilmanski <hilman@hilman.com>
2020-10-02 11:17:11 +0800

    v1-23 validation message to indonesia

---
+++ resources/lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Password kamu sudah direset!',
+    'sent' => 'Kami sudah mengirim email link password reset kamu!',
+    'throttled' => 'Mohon tunggu sebelum coba lagi.',
+    'token' => 'Password reset token ini tidak valid.',
+    'user' => "Tidak ada user dengan email tersebut.",
+
+];
