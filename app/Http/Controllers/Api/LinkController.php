1d3e6b7724a1388b6bbd9bc1f1c00c2979c072cf
Muhammad Juan Akbar <mail@mjuan.info>
2020-10-06 00:05:17 +0200

    [V1-7] add error report if add link failed, update resource lastupdate if new link added

--- app/Http/Controllers/Api/LinkController.php
+++ app/Http/Controllers/Api/LinkController.php
@@ -29,37 +29,60 @@ class LinkController extends AbstractApiController
 
         //check rss_id to get value of "media" and "user_id"
         $rss = Resource::findOrFail($request->rss_id);
+        $report = [];
+        $anyDataAdded = false;
         
         foreach($request->items as $item) {
-            if(!isValidUrl($item['link'])) 
-                return $this->responseNOK('url is not valid. url should be like this "https://pulo.dev/content-page"');
+            $isAllowedToSave = true;
+
+            if(!isValidUrl($item['link'])) {
+                $isAllowedToSave = false;
+                $report[$item['link']] = 'url is not valid. url should be like this "https://pulo.dev/content-page"';
+            }
             
             $cleanedUrl = cleanUrl($item['link']);
             $existedUrl = Link::where('url', $cleanedUrl)->first();
-            if ($existedUrl) 
-                return $this->responseNOK('link already exist');
+            if ($existedUrl) {
+                $isAllowedToSave = false;
+                $report[$item['link']] = 'link already exist';
+            }
             
-            $user = Auth::user();
-            $link = $user->links()->create([
-                'title' => $item['title'],
-                'url'  => $cleanedUrl,
-                'slug'  => generateSlug($item['title'], new Link),
-                'body'  => $item['description'],
-                'tags'  => $item['tags'] ?? '',
-                'owner'  => $item['owner'] ?? '',
-                'media'  => $rss->media,
-                'user_id' => $rss->user_id,
-                'original_published_at'  => $item['publishDate'] ?? Carbon::now(),
-            ]);  
-            
-            $status = true;
-            if (!$link)
-                $status = false;
+            if($isAllowedToSave) {
+                $user = Auth::user();
+                $link = $user->links()->create([
+                    'title' => $item['title'],
+                    'url'  => $cleanedUrl,
+                    'slug'  => generateSlug($item['title'], new Link),
+                    'body'  => $item['description'],
+                    'tags'  => $item['tags'] ?? '',
+                    'owner'  => $item['owner'] ?? '',
+                    'media'  => $rss->media,
+                    'user_id' => $rss->user_id,
+                    'original_published_at'  => $item['publishDate'] ?? Carbon::now(),
+                ]);  
+
+                if ($link) {
+                    $anyDataAdded = true;
+                } else {
+                    $report[$item['link']] = 'failed on saving data';
+                }
+
+                //TODO: write to logfile if add link failed
+            }
         }
 
-        if ($status)
-            return $this->responseOK();
+        if ($anyDataAdded) {
+            $rssUpdate = $rss->update([
+                'last_checked_at' => Carbon::now()
+            ]);
 
-        return $this->responseNOK('input link failed!');
+            //TODO: write to logfile if rss update failed
+        }
+        
+        if (sizeof($report) !== 0 ) {
+            return $this->responseNOK('there is some error on add link!', $report);
+        } else {
+            return $this->responseOK();
+        }
     }
 }
